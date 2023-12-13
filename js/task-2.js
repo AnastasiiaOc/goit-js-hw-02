af5e528f4d714c914c17d129a1207f90befb3511
Anastasiia Savinova Occhiminuti <anastesie@hotmail.com>
2023-12-13 22:44:37 +0100

    First task commit

---
+++ js/task-2.js
@@ -0,0 +1,18 @@
+function calculateTotal(number) {
+  let i= 1;
+  let total = 0;
+ // let total = temps +i;
+
+  while (i <= number){ 
+    total++;
+  }
+  return total;
+}
+calculateTotal(3);
+calculateTotal(0);
+calculateTotal(1);
+calculateTotal(4);
+
+
+
+console.log("hello");
